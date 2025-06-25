d6c16a326dc2cde86077d7beaaea1b69fc86dcd3
katarevaishnavi <156288695+katarevaishnavi@users.noreply.github.com>
2025-06-25 23:35:33 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,9 @@
+package com.vaishnavi;
+import java.util.*;
+public class conditional {
+    public static void main (String[] args) {
+        //if else statements
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
