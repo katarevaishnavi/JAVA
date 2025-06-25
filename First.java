fe7593d635365ee09f5523c2b347c33a32cd4bd0
katarevaishnavi <156288695+katarevaishnavi@users.noreply.github.com>
2025-06-25 23:22:17 +0530

    First.java committed

---
+++ First.java
@@ -0,0 +1,10 @@
+package com.vaishnavi;
+
+import java.net.SocketOption;
+
+public class First {
+    public static void main (String [] args)
+    {
+        System.out.println("Hello world");
+    }
+}
